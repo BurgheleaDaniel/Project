59ba2ceb88d7269f2efcede538974cf28b2b5bcc
bpaul25 <136641655+bpaul25@users.noreply.github.com>
2023-06-14 22:23:48 +0300

    Needs the vendor folder to work

---
+++ Backend Progress/project/index.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once ($_SERVER['DOCUMENT_ROOT'] . '/project/vendor/autoload.php');
+
+include_once "RestController.php";
+include_once "AuthController.php";
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+$authController = new AuthController($requestMethod);
+
+switch ($uri[2]) {
+    case 'LoginPage':
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $authController ->processRequest($username, $password);
+
+        header('Location: /project/HomePage/HomePage.html');
+
+        break;
+
+    default:
+
+        header('Location: /project/LoginPage/LoginPage.html');
+        exit();
+}
+
+////verify if the username and the password are correct
+
+//
+//$validUsername = 'admin';
+//$validPassword = 'pass';
+//
+//if ($username === $validUsername && $password === $validPassword) {
+//    // Redirect the user to the home page if the username and password are correct
+//    header('Location: ../HomePage/HomePage.html');
+//    exit;
+//} else {
+//    // Username or password is incorrect, show an error message
+//    echo 'Invalid username or password. Please try again.';
+//}
